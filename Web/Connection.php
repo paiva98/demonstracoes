327835963474ee1803a876028b3a557b3bd350eb
paiva98 <39721586+paiva98@users.noreply.github.com>
2020-12-14 14:10:47 -0300

    conexao

---
+++ Web/Connection.php
@@ -0,0 +1,12 @@
+<?php
+    abstract class Connection{
+        private static $conn;
+
+        public static function getConn(){
+            if(!self::$conn){
+                self::$conn = new PDO('mysql: host=localhost; dbname=mercado','root','');
+            }
+
+            return self::$conn;
+        }
+    }
